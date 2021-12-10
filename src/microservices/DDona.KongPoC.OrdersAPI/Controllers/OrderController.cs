d932290fceebaf094b16b3dae52ae56771f67940
Diego Doná <diedona@gmail.com>
2021-12-09 21:47:54 -0300

    Add project files.

---
+++ src/microservices/DDona.KongPoC.OrdersAPI/Controllers/OrderController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DDona.KongPoC.OrdersAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return Ok("A whole bunch of orders...");
+        }
+    }
+}
---
+++ src/microservices/DDona.KongPoC.ProductsAPI/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DDona.KongPoC.ProductsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IEnumerable<string> _Products = new[] { "Apple", "Banana", "Lemon" };
+
+        [HttpGet]
+        public ActionResult<string[]> Get()
+        {
+            return Ok(_Products);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            if (id < 0 || id >= _Products.Count())
+                return BadRequest("Out of bounds");
+
+            return Ok($"{id} - {_Products.ElementAt(id)}");
+        }
+    }
+}
